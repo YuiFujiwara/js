6b27fff06a3e9ed90c644877f6df47d3a527a01e
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-07 14:02:36 +0900

    Add files via upload
    関数 return

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 'use script';
 
-for (let i = 1; i <= 10; i++){
-  // if(i === 4){
-  // if(i % 3 === 0){
-  //   continue;
-  // }
-  if (i === 4){
-    break;
-  }
-  console.log(i);
+//関数
+
+function sum(a,b,c){
+  // console.log(a + b + c);
+  return a + b + c
 }
+
+// sum(1, 2, 3);
+// sum(3, 4, 5);
+
+const total = sum(1,2,3) + sum(3, 4, 5);
+// const total = 6 + 12; //18
+
+console.log(total);
