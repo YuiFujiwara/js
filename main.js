d60c5f69355c51b3d81544f014534fd4a9e07de8
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-07 15:15:27 +0900

    Add files via upload
    クラスの継承（親クラス、子クラス）

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 'use script';
 
 {
-//Scope
-
-  const x = 2;
-
-  function f() {
-    // const x = 1;
-    console.log(x);
+  class Player { //親クラス
+    constructor(name, score) { //メソッド
+      this.name = name;
+      this.score = score;
+    }
+    showInfo() {
+      console.log(`name: ${this.name} score: ${this.score}`);
+    }
   }
 
-  f();
-  console.log(x);
+  class SoccerPlayer extends Player { //子クラスタ
+    constructor(name, score, number) {
+     super(name,score);
+     this.number = number;
+  }
+  kick() {
+    console.log('Goooaaal!');
+  }
+}
+
+  const tsubasa = new SoccerPlayer('tsubasa', 99, 10);
+
+  tsubasa.kick();
+  console.log(tsubasa.number);
+  tsubasa.showInfo();
 }
